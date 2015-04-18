8864dfc15f5263556ca333cc4ffcb17614b63736
papapin <papapin777@gmail.com>
2015-04-18 15:24:28 +0300

    init

---
+++ server.js
@@ -0,0 +1,22 @@
+var User = require('./user');
+var db = require('db');
+var log = require('logger')(module);
+
+function run() {
+
+var vasya = new  User("Вася");
+var petya = new  User("Петя");
+
+vasya.hello(petya);
+
+log(db.getPhrase("Run successful"));
+
+}
+
+
+
+if (module.parent) {
+	exports.run = run
+} else {
+	run();
+}
---
+++ user/index.js
@@ -0,0 +1,16 @@
+var db = require('db');
+var log = require('logger')(module);
+
+db.connect();
+
+function User(name) {
+	this.name = name;
+}
+
+User.prototype.hello = function (who) {
+	log(db.getPhrase("Hello") + ", " + who.name);
+};
+
+module.exports = User;
+
+
